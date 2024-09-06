744f1c44344be69db6c8ea0412c8aa621940844b
grimazn6527 <42329771+seannlyj@users.noreply.github.com>
2024-09-07 00:45:48 +0800

    skeletal ui for login and signup screens

--- Skeleton/sunnyside/src/pages/Login.js
+++ Skeleton/sunnyside/src/pages/Login.js
@@ -7,29 +7,36 @@ function Login() {
       <div className="Logo">
         <img src={logo} alt="Logo" />
         <h1>SunnySide</h1>
-        <h2>brighten your day, no matter the weather</h2>
+        <h2>brighten your day,</h2>
+        <h2>no matter the weather</h2> 
       </div>
+
       <div className="LoginForm">
         <h1>Welcome Back</h1>
+
         <form>
-            <div>
-                <label htmlFor="email">Email:</label>
+            <div className="emailInputField">
+                <label htmlFor="email">E-mail</label>
                 <input type="email" id="email" name="email" required />
             </div>
+
             <div>
-                <label htmlFor="password">Password:</label>
+                <label htmlFor="password">Password</label>
                 <input type="password" id="password" name="password" required />
-                <div>
+                <div className="forgot-password">
                     <a href="/forgot-password">Forgot Password?</a>
                 </div>
             </div>
+
             <div>
                 <button type="submit">Log In</button>
             </div>
-            <div>
-                <a href="/signup">Don't have an account? Sign up</a>
+            <div className="signup-container">
+              <span className="signup-subtext">Don't have an account? </span>
+              <a href="/signup" className="sign-up">Sign up</a>
             </div>
         </form>
+
       </div>
     </div>
   );
--- Skeleton/sunnyside/src/pages/Signup.js
+++ Skeleton/sunnyside/src/pages/Signup.js
@@ -1,9 +1,60 @@
 import logo from '../art/sunnysidelogo.PNG';
+import './Signup.css';
 
 const SignUp = () => {
   return (
-    <div>
+    <div className="Signup">
+    <div className="Logo">
+      <img src={logo} alt="Logo" />
+      <h1>SunnySide</h1>
+      <h2>brighten your day,</h2>
+      <h2>no matter the weather</h2> 
     </div>
+    <div className="SignupForm">
+      <h1>Create Account</h1>
+      <form>
+          <div className="name-container">
+            <div className="firstname-container">
+              <label htmlFor="firstName">First Name</label>
+              <input type="text" id="firstName" name="firstName" required />
+            </div>
+            <div className="lastname-container">
+              <label htmlFor="lastName">Last Name</label>
+              <input type="text" id="lastName" name="lastName" required />
+            </div>
+          </div>
+
+          <div>
+              <label htmlFor="email">E-mail</label>
+              <input type="email" id="email" name="email" required />
+          </div>
+
+          <div className="whole-password-container">
+            <div className="password-container">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" required />
+            </div>
+            <div className="confirm-password-container">
+              <label htmlFor="confirmPassword">Confirm Password</label>
+              <input type="password" id="confirmPassword" name="confirmPassword" required />
+            </div>
+
+          </div>
+          
+          <div>
+              <p>You are agreeing to the Terms of Service and Privacy Policy.</p>
+          </div>
+
+          <div>
+              <button type="submit">Sign up</button>
+          </div>
+          <div className="login-container">
+              <span className="login-subtext">Already have an account? </span>
+              <a href="/login" className="log-in">Log in</a>
+            </div>
+      </form>
+    </div>
+  </div>
   );
 };
 
