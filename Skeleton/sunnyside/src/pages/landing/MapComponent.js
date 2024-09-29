7587c1d7b410ea6b59b7aee31eb2c20fe3c4c5bc
tgw6201 <103734987+tgw6201@users.noreply.github.com>
2024-09-29 11:57:16 +0800

    Added clustering feature for nearby locations as well as register button for the popup when hovering over

--- Skeleton/sunnyside/src/pages/landing/MapComponent.js
+++ Skeleton/sunnyside/src/pages/landing/MapComponent.js
@@ -1,19 +1,20 @@
 import React, { useEffect, useRef } from 'react';
 import { MapContainer, TileLayer, Marker, Popup, useMap } from 'react-leaflet';
-import L, { popup, Icon } from 'leaflet';
+import L, { Icon, divIcon } from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import "../../styling/MapComponent.css";
+import MarkerClusterGroup from 'react-leaflet-cluster';
 
 const MapComponent = () => {
   // Pinned locations should be passed as an array
   const markers = [
     {
         geocode: [1.3521, 103.8198],
-        popUp: "Test description for location 1"
+        popUp: "Name of activity for location 1"
     },
     {
         geocode: [1.3531, 103.9198],
-        popUp: "Test description for location 2"
+        popUp: "Name of activity for location 2"
     }
     ];
 
@@ -22,7 +23,22 @@ const MapComponent = () => {
     iconUrl: require('../../art/location-icons/location.png'),
     iconSize: [38, 38] // Size of the icon
   })
+
+  const createCustomClusterIcon = (cluster) => {
+    return new divIcon({
+      html: `<div class="cluster-icon">${cluster.getChildCount()}</div>`,
+      className: 'custom-marker-cluster',
+      iconSize: L.point(40, 40, true),
+    });
+  }
+
   const zoomControlRef = useRef(null); // Ref to hold the zoom control
+
+  // Function to handle register button click
+  const handleRegister = (location) => {
+    alert(`Registered for ${location}`);
+    // You can add your logic here, such as calling an API or updating a state
+  };
   
 
   // Custom component to add zoom control to the bottom left
@@ -53,12 +69,21 @@ const MapComponent = () => {
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {markers.map(marker => (
-      <Marker position={marker.geocode} icon={customIcon}>
-      </Marker>
-      ))
-      };
 
+      <MarkerClusterGroup
+        chunkedLoading
+        iconCreateFunction={createCustomClusterIcon}
+      >
+        {markers.map(marker => (
+        <Marker position={marker.geocode} icon={customIcon}>
+          <Popup>
+            <h2>{marker.popUp}</h2>
+            <button className="register-button" onClick={() => handleRegister(marker.popUp)}>Register</button>
+          </Popup>
+        </Marker>
+        ))
+        };
+      </MarkerClusterGroup>
       {/* Add custom zoom control to bottom left */}
       <CustomZoomControl />
     </MapContainer>
